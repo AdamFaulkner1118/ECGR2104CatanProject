801e608cfe27658d659a9b599f9130f5a692601f
“Adam <afaulkn5@uncc.edu>
2019-04-30 21:44:52 +0000

    RosasWork

--- GameBoard.cpp
+++ GameBoard.cpp
@@ -1,1 +1,102 @@
 #include "GameBoard.cpp"
+
+void GameBoard::setLand(LandType L){
+    land = L;
+}
+void GameBoard::setResource(ResourcesType R){
+    resources = R;
+}
+void GameBoard::setNumber(int n){
+    num = n;
+}
+
+void GameBoard::setPlayer(string pname){
+    name=pname;
+}
+
+LandType GameBoard::getLand(){
+    return land;
+}
+
+ResourcesType GameBoard::getResource(){
+    return resources;
+}
+int GameBoard::getNumber(){
+    return num;
+}
+
+string GameBoard::getPlayer(){
+    return name;
+}
+
+
+string GameBoard::render(int line){
+    stringstream ss;
+    switch(line){
+        case 0: return " ------------- ";
+        case 1: return "|             |";
+        case 2: return "|             |";
+        case 3: return "|             |";
+        case 4: return "|             |";
+        case 5: return "|             |";
+        case 6: return "|             |";
+        case 7: return " ------------- ";
+        default:
+            return " ";
+    }
+}
+
+Tile::Tile(LandType L, ResourcesType R, int n, string pname){
+    setLand(L);
+    setResource(R);
+    setNumber(n);
+    setPlayer(pname);
+}
+
+string Tile::render(int line){
+    stringstream ss;
+    switch(line){
+        case 0: return " ------------- ";
+        case 1: 
+            switch(land){
+            case FOREST:
+                return "|Forest       |";
+                break;
+            case HILLS:
+                return "|Hills        |";
+                break;
+            case FIELD:
+                return "|Field        |";
+                break;
+            case PASTURE:
+                return "|Pasture      |";
+                break;
+            case MOUNTAIN:
+                return "|Mountain     |";
+                break;
+            default:
+                return "|             |";
+                break;
+            break;
+            }
+        case 2:
+            if(num<=9){
+            ss <<  "|            "<<num<<"|";
+            return ss.str();
+            break;
+            }
+            if(num>9){
+            ss <<  "|           "<<num<<"|";
+            return ss.str();
+            break;
+            }
+                
+        case 3: return "|             |";
+        case 4: return "|             |";
+        case 5: return "|             |";
+        case 6: return "|             |";
+        case 7: return " ------------- ";
+        default:
+            return " ";
+    } 
+}
--- GameBoard.h
+++ GameBoard.h
@@ -1,9 +1,46 @@
-#ifndef GAMEBOARD_H
-#define GAMEBOARD_H
+for bordgame.h
 
 #include <iostream>
-#include "Tile.h"
+#include <sstream>
+#include <string>
+//#include "tile.h"
 
+using namespace std;
 
+enum LandType{FOREST, HILLS, FIELD, PASTURE, MOUNTAIN};
+enum ResourcesType{WOOD, BIRCKS, GRAIN, WOOL, ORE};
+
+class GameBoard{
+    protected:
+    //describes type of land
+    LandType land;
+    //describes type of resource
+    ResourcesType resources;
+    //describe number associated
+    int num;
+    string name;
+    
+    public:
+    void setLand(LandType);
+    void setResource(ResourcesType);
+    void setNumber(int);
+    LandType getLand();
+    ResourcesType getResource();
+    int getNumber();
+    //Need to set player
+    void setPlayer(string);
+    string getPlayer();
+    //Need to set if its a settlement or city
+    
+
+    virtual string render(int); 
+   
+};
+
+class Tile : public GameBoard{
+    public:
+    Tile(LandType, ResourcesType, int, string);
+    string render(int);
+};
 
 #endif
